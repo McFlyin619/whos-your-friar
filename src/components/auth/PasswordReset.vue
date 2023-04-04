6c18df8702a2880fa7bb3f4fbe0718dc68f81cda
obrien-avatech <obrien.mcquade@avatechnologyllc.com>
2023-04-04 08:40:04 -0700

    update

--- src/components/auth/PasswordReset.vue
+++ src/components/auth/PasswordReset.vue
@@ -2,7 +2,12 @@
 	<div>
 		<div v-if="!success">
 			<div class="form-floating mb-3 mt-3">
-				<input v-model="email" type="text" class="form-control" placeholder="Username or Email" />
+				<input
+					v-model="email"
+					type="text"
+					class="form-control"
+					placeholder="Username or Email"
+				/>
 				<label>Enter your email to reset your password</label>
 			</div>
 			<button :disabled="isLoading" class="btn sd-btn-dark me-4">
@@ -10,9 +15,18 @@
 					<i class="fa-solid fa-baseball fa-spin"></i>
 					Loading...
 				</div>
-				<div v-show="!isLoading" @click="resetEmail">Reset Password</div>
+				<div v-show="!isLoading" @click="resetEmail">
+					Reset Password
+				</div>
 			</button>
-			<p>Oops need to login?<a role="button" class="ms-1 link" @click="$emit('resetForm')">Go back</a></p>
+			<p>
+				Oops need to login?<a
+					role="button"
+					class="ms-1 link"
+					@click="$emit('resetForm')"
+					>Go back</a
+				>
+			</p>
 		</div>
 		<div v-else>
 			<h4>Email sent!</h4>
@@ -20,8 +34,12 @@
 	</div>
 </template>
 <script>
+import Parse from 'parse';
 // eslint-disable-next-line no-undef
-Parse.initialize('NpUNjfi37nXWf6TcBlVjEgbZkrfGlwQW7sPlAbph', '9TZO5xhE4Bg8lyrAG7maKvOPPa9DsB86HQZUGn27');
+Parse.initialize(
+	'NpUNjfi37nXWf6TcBlVjEgbZkrfGlwQW7sPlAbph',
+	'9TZO5xhE4Bg8lyrAG7maKvOPPa9DsB86HQZUGn27'
+);
 // eslint-disable-next-line no-undef
 Parse.serverURL = 'https://mlbotl.b4a.io/';
 export default {
@@ -44,7 +62,12 @@ export default {
 					console.log('Password reset request was sent successfully');
 				})
 				.catch((error) => {
-					console.log('The login failed with error: ' + error.code + ' ' + error.message);
+					console.log(
+						'The login failed with error: ' +
+							error.code +
+							' ' +
+							error.message
+					);
 				});
 		},
 	},
